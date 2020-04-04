7e6c0472b8bc32c6dbcb747bf8810e567bd71971
Liberlay <liberlay555@gmail.com>
2020-04-04 19:49:54 +0300

    Lab3

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -9,88 +9,123 @@
 
 using namespace std;
 
-int main()
+// Задача 19 среднего уровня
+void task19() {
+
+
+	char ch[15]; // Hello Wordl Oh
+	cout << "Enter 14 symbols:";
+	cin.getline(ch, 15);
+
+	int lengthArray = 0;
+	char** separateWords = 0;
+	char* tokens = strtok(ch, " ");
+	while (tokens)
+	{
+		separateWords = (char**)realloc(separateWords, sizeof(char*) * ++lengthArray);
+		separateWords[lengthArray - 1] = (char*)malloc(strlen(tokens) + 1);
+		strcpy(separateWords[lengthArray - 1], tokens);
+		tokens = strtok(0, " ");
+	}
+	for (int i = lengthArray - 1; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (strcmp(separateWords[j], separateWords[i]) > 0)
+			{
+				char* t = separateWords[j];
+				separateWords[j] = separateWords[i];
+				separateWords[i] = t;
+			}
+		}
+	}
+
+	for (int i = 0; i < lengthArray; i++) {
+		cout << separateWords[i] << " ";
+	}
+
+}
+
+// Задача 11 среднего уровня
+void task11()
 {
 
-    char chA[100]; // bbb aaa aaaabba baba
-    cout << "Enter 20 symbols with a:", cin >> chA;
-    int countNew = 0, countOld = 0;
+	char chA[100]; // bbb aaa aaaabba baba
+	cout << "Enter 20 symbols with a:", cin >> chA;
+	int countNew = 0, countOld = 0;
 
-    for (int i = 0; i < strlen(chA); i++) {
-        if (chA[i] == 'a') {
-            countNew++;
-        }
-        else if (countNew > countOld) {
-            countOld = countNew;
-            countNew = 0;
-        }
-        else {
-            countNew = 0;
-        }
-    }
+	for (int i = 0; i < strlen(chA); i++) {
+		if (chA[i] == 'a') {
+			countNew++;
+		}
+		else if (countNew > countOld) {
+			countOld = countNew;
+			countNew = 0;
+		}
+		else {
+			countNew = 0;
+		}
+	}
 
-    cout << "Count of A: " << countOld;
+	cout << "Count of A: " << countOld;
+}
+
+// Задача 19 выского уровня
+void task193() {
+	//char str[200] = "Function strtok searches for tokens in a row string. The sequence of calls to this function, a line break string into tokens, which represent a sequence of symbols, separated by comma";
+	char str[200] = "noitcnuF kotrts sehcraes rof snekot ni a wor gnirts. ehT ecneuqes fo sllac ot siht noitcnuf, a enil kaerb gnirts otni snekot, hcihw tneserper a ecneuqes fo slobmys, detarapes yb ammoc";
+	cout << "Each word of this string need to be reversed:\n" << str;
+
+	int ArrayLength = 0;
+	char** ReverseWords = 0;
+	char* token = strtok(str, " ");
+
+	while (token) {
+		ReverseWords = (char**)realloc(ReverseWords, sizeof(char*) * ++ArrayLength);
+		ReverseWords[ArrayLength - 1] = (char*)malloc(strlen(token) + 1);
+		strcpy(ReverseWords[ArrayLength - 1], token);
+		token = strtok(0, " ");
+	}
+
+	cout << "\n\nReversed string:\n";
+
+	for (int i = 0; i < ArrayLength; i++) {
+		string str(ReverseWords[i]);
+		if (str.back() == '.' || str.back() == ',') {
+			reverse(str.begin(), str.end() - 1);
+		}
+		else {
+			reverse(str.begin(), str.end());
+		}
+
+		cout << str << " ";
+	}
+
+}
 
 
+void chooseTask() {
+	int taskNumber;
+	cout << "Choose task number(19, 11, 193):";
+	cin >> taskNumber;
+	switch (taskNumber) {
+	case 19:
+		task19();
+		break;
 
-    char ch[15]; // Hello Wordl Oh
-    cout << "Enter 14 symbols:";
-    cin.getline(ch, 15);
+	case 11:
+		task11();
+		break;
 
-    int lengthArray = 0;
-    char** separateWords = 0;
-    char* tokens = strtok(ch, " ");
-    while (tokens)
-    {
-        separateWords = (char**)realloc(separateWords, sizeof(char*) * ++lengthArray);
-        separateWords[lengthArray - 1] = (char*)malloc(strlen(tokens) + 1);
-        strcpy(separateWords[lengthArray - 1], tokens);
-        tokens = strtok(0, " ");
-    }
-    for (int i = lengthArray - 1; i > 0; i--) {
-        for (int j = 0; j < i; j++) {
-            if (strcmp(separateWords[j], separateWords[i]) > 0)
-            {
-                char* t = separateWords[j];
-                separateWords[j] = separateWords[i];
-                separateWords[i] = t;
-            }
-        }
-    }
+	case 193:
+		task193();
+		break;
 
-    for (int i = 0; i < lengthArray; i++) {
-        cout << separateWords[i] << " ";
-    }
+	default:
+		cout << "Enter task number from list!\n";
+		chooseTask();
+	}
+}
 
-
-
-    //char str[200] = "Function strtok searches for tokens in a row string. The sequence of calls to this function, a line break string into tokens, which represent a sequence of symbols, separated by comma";
-    char str[200] = "noitcnuF kotrts sehcraes rof snekot ni a wor gnirts. ehT ecneuqes fo sllac ot siht noitcnuf, a enil kaerb gnirts otni snekot, hcihw tneserper a ecneuqes fo slobmys, detarapes yb ammoc";
-    cout << "Each word of this string need to be reversed:\n" << str;
-
-    int ArrayLength = 0;
-    char** ReverseWords = 0;
-    char* token = strtok(str, " ");
-
-    while (token) {
-        ReverseWords = (char**)realloc(ReverseWords, sizeof(char*) * ++ArrayLength);
-        ReverseWords[ArrayLength - 1] = (char*)malloc(strlen(token) + 1);
-        strcpy(ReverseWords[ArrayLength - 1], token);
-        token = strtok(0, " ");
-    }
-
-    cout << "\n\nReversed string:\n";
-
-    for (int i = 0; i < ArrayLength; i++) {
-        string str(ReverseWords[i]);
-        if (str.back() == '.' || str.back() == ',') {
-            reverse(str.begin(), str.end()-1);
-        }
-        else {
-            reverse(str.begin(), str.end());
-        }
-
-        cout << str << " ";
-    }
-
+int main()
+{
+	chooseTask();
 }
